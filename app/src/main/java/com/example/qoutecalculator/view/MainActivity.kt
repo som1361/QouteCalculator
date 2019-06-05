1860b57c08caf5d447c5ba06537099ce1672c31d
Somayyeh Zahedian <somy@bountye.com>
2019-06-05 14:16:59 +1000

    authentication dialog is shown now.

--- app/src/main/java/com/example/qoutecalculator/view/MainActivity.kt
+++ app/src/main/java/com/example/qoutecalculator/view/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.qoutecalculator.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
 import com.example.qoutecalculator.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.authentication_dialog.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +18,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun respondToClicks() {
+      calcQouteButton.setOnClickListener { showAuthenticationDialog() }
+    }
 
+    private fun showAuthenticationDialog() {
+val mAuthDialog = LayoutInflater.from(this).inflate(R.layout.authentication_dialog, null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mAuthDialog)
+        val mAlertDialog = mBuilder.show()
+
+        application_btn.setOnClickListener {
+
+        }
+
+        login_btn.setOnClickListener {
+            
+        }
     }
 
     private fun loadView() {
